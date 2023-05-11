d53f570187f4429a25f704b61a9a76692a484dd1
Ariel <arielmarno@outlook.com>
2023-05-11 11:40:30 -0300

    modifique una fila que extendia el margen derecho en la adaptacion responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
       <div className="footer">
-        <div className="row">
+        <div>
               <div className="icons" id="contact">
                 <a href="https://github.com/ArielMarno" target="_blank"><img className="social" src="https://cdn3.iconfinder.com/data/icons/sociocons/128/github-sociocon.png"/></a>
                 <a href="https://www.linkedin.com/in/ariel-alejandro-marchesano-97729b26b/" target="_blank"><img className="social" src="https://cdn3.iconfinder.com/data/icons/sociocons/128/linkedin-sociocon.png"/></a>
